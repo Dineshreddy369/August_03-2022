b08af2916d899525935e0ee5bcb170c1b05d521c
Dineshreddy369 <111274170+Dineshreddy369@users.noreply.github.com>
2022-08-15 14:01:36 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,61 @@
+// Create 20 Variables in a JS file and print it
+
+var city="Hyderabad";
+console.log(city);
+
+var gender="Male";
+console.log(gender);
+
+var product="Mobile";
+console.log(product);
+
+var brand="Apple";
+console.log(brand);
+
+var company="Google";
+console.log(company);
+
+var language="Python";
+console.log(language);
+
+var laptop="Dell";
+console.log(laptop);
+
+var mobile="Vivo";
+console.log(mobile);
+
+var country="India";
+console.log(country);
+
+var game="Cricket";
+console.log(game);
+
+var atom="Photon";
+console.log(atom);
+
+var bank="Axis";
+console.log(bank);
+
+var socialmedia="YouTube";
+console.log(socialmedia);
+
+var currency="Rupee";
+console.log(currency);
+
+var forest="Sesachalam";
+console.log(forest);
+
+var app="Cricbuzz";
+console.log(app);
+
+var education="Udemy";
+console.log(education);
+
+var season="Summer";
+console.log(season);
+
+var timing="IST";
+console.log(timing);
+
+var day="Sunday";
+console.log(day);
